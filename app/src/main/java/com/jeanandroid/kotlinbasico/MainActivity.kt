6aa3c3a508c0a9ce64cfaa429896f58f036e4862
jeanMic <jean_01_michel@hotmail.com>
2018-06-14 00:26:36 -0300

    Layout main adicionado.

--- app/src/main/java/com/jeanandroid/kotlinbasico/MainActivity.kt
+++ app/src/main/java/com/jeanandroid/kotlinbasico/MainActivity.kt
@@ -1,12 +1,23 @@
 package com.jeanandroid.kotlinbasico
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.view.View
+import android.widget.ImageView
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity() , View.OnClickListener {
+
+    var ImgTrab: ImageView? = null
+    var ImsCasa: ImageView? = null
+    var ImgConfig: ImageView? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+    }
+
+    override fun onClick(v: View?) {
+
     }
 }
