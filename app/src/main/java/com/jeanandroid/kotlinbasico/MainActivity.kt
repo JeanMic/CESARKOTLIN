9e917cbda7c74fd30c3db8091bc134a91fbcefce
jeanMic <jean_01_michel@hotmail.com>
2018-06-14 07:39:43 -0300

    Adicionado esculta na imagem trabalho.

--- app/src/main/java/com/jeanandroid/kotlinbasico/MainActivity.kt
+++ app/src/main/java/com/jeanandroid/kotlinbasico/MainActivity.kt
@@ -1,23 +1,39 @@
 package com.jeanandroid.kotlinbasico
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.view.View
-import android.widget.ImageView
+import com.jeanandroid.kotlinbasico.listas.listTarefasTrabalho
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() , View.OnClickListener {
-
-    var ImgTrab: ImageView? = null
-    var ImsCasa: ImageView? = null
-    var ImgConfig: ImageView? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        ImgTrab?.setOnClickListener(this)
+        ImgCasa?.setOnClickListener(this)
+        ImgConfig?.setOnClickListener(this)
     }
 
-    override fun onClick(v: View?) {
+    override fun onClick(view: View?) {
+
+        when (view) {
+
+            ImgTrab -> {
+                val intentTrabalho = Intent(this, listTarefasTrabalho::class.java)
+                startActivity(intentTrabalho)
+            }
+//            R.id.ImgCasa -> {
+//                val intentcasa = Intent(this, listTarefasCasa::class.java)
+//                startActivity(intentcasa)
+//            }
+//            R.id.ImgConfig -> {
+//            }
+        }//                Intent CategoriasPesquisaProdutos = new Intent(getApplicationContext(), telaPesquisaCategoriasProduto.class);
+        //                startActivity(CategoriasPesquisaProdutos);
 
     }
 }
---
+++ app/src/main/java/com/jeanandroid/kotlinbasico/listas/listTarefasTrabalho.kt
@@ -0,0 +1,15 @@
+package com.jeanandroid.kotlinbasico.listas
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.jeanandroid.kotlinbasico.R
+
+class listTarefasTrabalho : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_list_tarefas_trabalho)
+
+
+    }
+}
